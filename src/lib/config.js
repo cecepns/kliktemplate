b9f6c76d82cb352a3893e243d6ae3b1c0d1e97d0
Cecep Nandang <cecep@kima.one>
2026-03-10 01:55:32 +0700

    feat: init app

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,2 +1,4 @@
-export const API_URL = '/api'
-export const UPLOADS_URL = '/uploads-web-template'
+const BASE_URL = 'https://api-inventory.isavralabel.com/kliktemplate'
+
+export const API_URL = `${BASE_URL}/api`
+export const UPLOADS_URL = `${BASE_URL}/uploads-web-template`
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const API_TARGET = 'https://api-inventory.isavralabel.com/kliktemplate'
-
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    proxy: {
-      '/api': { target: API_TARGET, changeOrigin: true },
-      '/uploads-web-template': { target: API_TARGET, changeOrigin: true },
-    },
   },
 })
