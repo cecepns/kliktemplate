37f986f8671cfa48d7ca6be455c4c1d5a19f6992
Rispan <rispanrobidigjaya0@gmail.com>
2026-03-11 20:07:02 +0700

    fix: bug

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -9,7 +9,7 @@ export default function Layout() {
     <div className="min-h-screen bg-gradient-to-b from-sky-50 to-blue-50/60">
       <Header onMenuClick={() => setSidebarOpen((v) => !v)} />
       <Sidebar open={sidebarOpen} onClose={() => setSidebarOpen(false)} />
-      <main className="flex min-h-[calc(100vh-3.5rem)] flex-col pt-4 pl-0 lg:pl-64">
+      <main className="flex min-h-[calc(100vh-3.5rem)] flex-col pt-4 pl-0 lg:pl-64 overflow-x-hidden">
         <div className="flex min-h-full flex-1 flex-col p-4 sm:p-6">
           <Outlet />
         </div>
